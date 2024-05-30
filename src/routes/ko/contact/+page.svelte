7a023048fd69bf1226a555fed1a97ce91c2e92e2
98yejin <o0o2211915@gmail.com>
2024-05-31 00:48:36 +0900

    contact 업데이트

--- src/routes/ko/contact/+page.svelte
+++ src/routes/ko/contact/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	// @ts-nocheck
 </script>
 
 <svelte:head>
@@ -20,8 +19,51 @@
 			height="60"
 			style="margin-right: 0.5rem;"
 		/>
-		EMAIL : 'mailto:o0o2211915@gmail.com'
+		EMAIL :<a href="mailto:o0o2211915@gmail.com" style="margin-left: 1rem; color:black"
+			>o0o2211915@gmail.com</a
+		>
 	</h1>
+	<h1 class="subtitle">
+		<img
+			src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Globe%20Showing%20Asia-Australia.png"
+			alt="Globe Showing Asia-Australia"
+			width="60"
+			height="60"
+			style="margin-right: 0.5rem;"
+		/>
+		WEBSITE :<a href="https://98yejin.github.io/" style="margin-left: 1rem; color:black"
+			>https://98yejin.github.io</a
+		>
+	</h1>
+	<h1 class="subtitle">
+		<img
+			src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Cat.png"
+			alt="Cat"
+			width="60"
+			height="60"
+			style="margin-right: 0.5rem;"
+		/>
+		GITHUB :<a href="https://github.com/98yejin" style="margin-left: 1rem; color:black"
+			>https://github.com/98yejin</a
+		>
+	</h1>
+	<h1 class="subtitle">
+		<img
+			src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Identification%20Card.png"
+			alt="Identification Card"
+			width="60"
+			height="60"
+			style="margin-right: 0.5rem;"
+		/>
+		LinkedIn :<a
+			href="https://linkedin.com/in/yejin-park-28a3a314b"
+			style="margin-left: 1rem; color:black">https://linkedin.com/in/yejin-park-28a3a314b</a
+		>
+	</h1>
+	<div class="white-box">
+		<p>궁금하거나 논의하고 싶은 내용이 있다면 편하게 연락주세요!</p>
+		<p>커피챗도 환영합니다 😀</p>
+	</div>
 </div>
 
 <style>
