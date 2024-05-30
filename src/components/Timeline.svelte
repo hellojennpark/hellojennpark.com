a04e1e8d180c3cfe9e778d58c8a8ee9bed404b42
98yejin <o0o2211915@gmail.com>
2024-05-30 22:24:20 +0900

    커리어페이지 업데이트

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,81 @@
+<script>
+	/**
+	 * @type {any[]}
+	 */
+	export let events = [];
+</script>
+
+<div>
+	<h1 class="subtitle">
+		<img
+			src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Calendar.png"
+			alt="Calendar"
+			width="60"
+			height="60"
+			style="margin-right: 0.5rem;"
+		/>타임라인
+	</h1>
+</div>
+<div class="timeline">
+	{#each events as event, index}
+		<div class="event {index === events.length - 1 ? 'last' : ''}">
+			<div class="company">{event.company}<span class="jobtype">{event.jobtype}</span></div>
+			<div class="role">{event.role}</div>
+			<div class="team">{event.team}</div>
+			<div class="date">{event.date}</div>
+		</div>
+		{index === events.length - 1 ? '' : '>'}
+	{/each}
+</div>
+
+<style>
+	.timeline {
+		display: flex;
+		overflow-x: auto;
+		white-space: nowrap;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.event {
+		background-color: rgba(243, 243, 243);
+		padding: 20px;
+		border-radius: 5px;
+		display: inline-block;
+		position: relative;
+		margin: 20px 20px;
+		text-align: left;
+	}
+
+	.event:first-child::before {
+		display: none;
+	}
+
+	.event.last {
+		background-color: rgba(255, 230, 0, 0.75);
+	}
+
+	.event .date {
+		font-size: 1rem;
+	}
+
+	.event .company {
+		font-size: 1.2rem;
+		margin-bottom: 0.4rem;
+	}
+
+	.event .jobtype {
+		margin-left: 0.2rem;
+		font-size: 0.8rem;
+	}
+
+	.event .role {
+		font-size: 1rem;
+		margin-bottom: 0.4rem;
+	}
+
+	.event .team {
+		font-size: 1rem;
+		margin-bottom: 0.4rem;
+	}
+</style>
---
+++ src/routes/ko/career/+page.svelte
@@ -0,0 +1,76 @@
+<script>
+	import { List, Li } from 'flowbite-svelte';
+	import Timeline from '../../../components/Timeline.svelte';
+
+	const events = [
+		{
+			company: 'ESTsecurity',
+			jobtype: '현장실습',
+			date: '2020년 7월 - 2021년 2월 · 8개월',
+			role: 'DevOps Engineer',
+			team: 'PMS-MPI Cell',
+			descriptions: []
+		},
+		{
+			company: 'PearlAbyss',
+			jobtype: '정규직',
+			date: '2021년 7월 - 2022년 12월 · 1년 6개월',
+			role: 'Software Engineer',
+			team: 'Platform Programming Team',
+			descriptions: []
+		},
+		{
+			company: 'KakaoPay',
+			jobtype: '정규직',
+			date: '2022년 12월 - 현재 · 1년 6개월',
+			role: 'Software Engineer',
+			team: 'SRE Team Release Engineering Part',
+			descriptions: []
+		}
+	];
+</script>
+
+<svelte:head>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Jersey+10+Charted&display=swap"
+		rel="stylesheet"
+	/>
+	<title>Yejin Park (Career)</title>
+	<meta name="description" content="커리어" />
+</svelte:head>
+
+<div>
+	<Timeline {events} />
+	<div>
+		<h1 class="subtitle">
+			<img
+				src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Bookmark%20Tabs.png"
+				alt="Bookmark Tabs"
+				width="60"
+				height="60"
+				style="margin-right: 0.5rem;"
+			/>경력사항
+		</h1>
+	</div>
+	<div class="white-box">
+		{#each events.slice().reverse() as event, index}
+			<div>
+				<p class="bolder">{index + 1}.{event.company}</p>
+				<List list="decimal">
+					<Li><p>{event.role}, {event.team}</p></Li>
+					<Li><p>{event.date}</p></Li>
+					{#each event.descriptions as description}
+						<Li><p>{description}</p></Li>
+					{/each}
+				</List>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.bolder {
+		font-weight: 800;
+		font-size: 1.5rem;
+	}
+</style>
