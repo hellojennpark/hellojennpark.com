48b188f52d08e41c3084ec5183b130b82d81d9b6
98yejin <o0o2211915@gmail.com>
2025-05-13 00:11:48 -0400

    Refactor Hero component layout and improve HeroBackground image styling

--- src/pages/components/Hero.tsx
+++ src/pages/components/Hero.tsx
@@ -3,13 +3,13 @@ import { HeroBackground } from "./HeroBackground";
 export default function Hero() {
   return (
     <section className="relative min-h-[100vh] flex items-center justify-center text-white">
-      <HeroBackground silhouetteColor="ffc8dd" opacity={0.9} />
-      <div className="relative z-10 text-center">
+      <div className="relative z-10 text-center px-10">
         <h1 className="text-4xl font-bold">{"Welcome, I'm Jenn."}</h1>
         <p className="text-xl mt-2">
           {"I build tools that help developers work with joy and flow."}
         </p>
       </div>
+      <HeroBackground silhouetteColor="ffc8dd" opacity={0.9} />
     </section>
   );
 }
--- src/pages/components/HeroBackground.tsx
+++ src/pages/components/HeroBackground.tsx
@@ -13,17 +13,21 @@ export const HeroBackground = ({
         <img
           src="/toroto.png"
           alt="Toronto skyline"
-          className="w-full h-auto object-cover"
+          className="
+    w-full h-full
+    object-contain object-bottom
+    sm:object-cover sm:object-top
+  "
           style={{
             opacity: opacity,
-            mixBlendMode: "multiply", // 또는 "screen", "overlay", 등도 테스트 가능
+            mixBlendMode: "multiply",
           }}
         />
         <div
           className="absolute inset-0 w-full h-full"
           style={{
             backgroundColor: `#${silhouetteColor}`,
-            mixBlendMode: "color", // 또는 "color", "overlay", 등 시도해볼 수 있음
+            mixBlendMode: "color",
             opacity: 1,
           }}
         />
