40834e24f5fa397831d1760083c6281fbf0e830f
98yejin <o0o2211915@gmail.com>
2025-05-13 16:46:36 -0400

    Add Google Font Nunito, update document head with favicon and title, and wrap main content in a styled main element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "./components/Header";
+import { Nunito } from "next/font/google";
+import Head from "next/head";
+
+const nunito = Nunito({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Header />
-      <Component {...pageProps} />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>HelloJennPark</title>
+      </Head>
+      <main className={nunito.className}>
+        <Header />
+        <Component {...pageProps} />
+      </main>
     </div>
   );
 }
