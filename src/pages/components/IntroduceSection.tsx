5a1840770e71036e6439b2bf3e15403adaf588a0
98yejin <o0o2211915@gmail.com>
2025-05-14 15:57:55 -0400

    Update IntroduceSection styling for improved responsiveness and adjust typewriter speed for better user experience

--- src/pages/components/IntroduceSection.tsx
+++ src/pages/components/IntroduceSection.tsx
@@ -11,7 +11,10 @@ export default function IntroduceSection() {
       id="introduce"
       className="relative min-h-[100dvh] flex flex-col items-start px-10 justify-center snap-start shadow-[0_8px_20px_-10px_rgba(0,0,0,0.3)]"
     >
-      <div className="text-5xl  space-y-5" style={{ color: primaryColor }}>
+      <div
+        className="text-3xl md:text-5xl space-y-5"
+        style={{ color: primaryColor }}
+      >
         <div>Hello, I’m Jenn.</div>
         <div className="border">
           <Typewriter
@@ -22,7 +25,7 @@ export default function IntroduceSection() {
             loop={false}
             cursor
             cursorStyle="|"
-            typeSpeed={100}
+            typeSpeed={80}
             deleteSpeed={50}
             delaySpeed={1500}
           />
