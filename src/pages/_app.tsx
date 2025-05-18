2c38002eda0891f834d51b558ef9896fdf768a58
98yejin <o0o2211915@gmail.com>
2025-05-17 23:51:40 -0400

    Add @vercel/analytics package and integrate analytics component in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Nunito } from "next/font/google";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Header />
         <Component {...pageProps} />
       </main>
+      <Analytics />
     </div>
   );
 }
