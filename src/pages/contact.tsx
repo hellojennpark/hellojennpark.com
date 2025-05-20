ba5bb8f3c1a1d8a952e2f1768b380f51f3cc39b4
98yejin <o0o2211915@gmail.com>
2025-05-20 12:58:15 -0400

    Add Contact page with email and social links, utilizing PageLayout for consistent styling

---
+++ src/pages/contact.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import PageLayout from "@/components/layout/PageLayout";
+import { useTimeThemeStore } from "@/store/useTimeThemeStore";
+import clsx from "clsx";
+import { Mail, Github, Linkedin } from "lucide-react";
+import Link from "next/link";
+
+export default function ContactPage() {
+  const { timeOfDay } = useTimeThemeStore();
+  const isNight = timeOfDay == "night";
+  const timeOfDayStyle = isNight
+    ? "bg-gray-900 text-white border-white"
+    : "bg-white text-black border-black";
+
+  return (
+    <PageLayout>
+      <div className="flex text-xl md:text-2xl font-semibold">
+        <span className="mr-2">Contact</span>
+      </div>
+
+      {/* Introduction */}
+      <p className="mb-8 max-w-2xl">
+        {
+          "In Toronto these days. Feel free to reach out with any questions, ideas, or just to say hi."
+        }
+      </p>
+
+      <div className={clsx("space-y-4 border rounded-lg p-5", timeOfDayStyle)}>
+        <div className="flex items-center">
+          <Mail className="w-4 h-4 mr-2" />
+          <Link href="mailto:hellojennpark@gmail.com">
+            hellojennpark@gmail.com
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <Github className="w-4 h-4 mr-2" />
+          <Link href="https://github.com/hellojennpark" target="_blank">
+            github.com/hellojennpark
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <Linkedin className="w-4 h-4 mr-2" />
+          <Link
+            href="https://www.linkedin.com/in/hellojennpark"
+            target="_blank"
+          >
+            linkedin.com/in/hellojennpark
+          </Link>
+        </div>
+      </div>
+    </PageLayout>
+  );
+}
