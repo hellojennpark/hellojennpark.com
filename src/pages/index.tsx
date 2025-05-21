8b7860fc004644eb27a1e7aa599fdf080809e9f5
98yejin <o0o2211915@gmail.com>
2025-05-21 00:36:17 -0400

    Refactor Home component by removing state management and popstate event handling for section scrolling, simplifying the structure and improving readability.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { Footer } from "@/components/Footer";
 import IntroduceSection from "@/components/IntroduceSection";
 import CareerSection from "@/components/CareerSection";
@@ -9,28 +8,8 @@ import EtcSection from "@/components/EtcSection";
 import Hero from "@/components/Hero";
 
 export default function Home() {
-  const [key, setKey] = useState(0);
-
-  useEffect(() => {
-    const handlePopState = () => {
-      setKey((prev) => prev + 1);
-      const hash = window.location.hash || "#welcome";
-      const targetSection = document.getElementById(hash.replace("#", ""));
-      if (targetSection) {
-        targetSection.scrollIntoView({ behavior: "smooth" });
-      }
-    };
-
-    window.addEventListener("popstate", handlePopState);
-
-    return () => window.removeEventListener("popstate", handlePopState);
-  }, []);
-
   return (
-    <div
-      key={key}
-      className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory"
-    >
+    <div className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory">
       <Hero />
       <IntroduceSection />
       <CareerSection />
