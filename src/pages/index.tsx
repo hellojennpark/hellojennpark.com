def6ba12f9b7035d91d452fa260731cf0f18fc52
98yejin <o0o2211915@gmail.com>
2025-05-29 22:23:59 -0400

    fix scroll bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory">
+    <div className="h-screen overflow-y-scroll scroll-smooth snap-y snap-proximity">
       <Hero />
       <IntroduceSection />
       <CareerSection />
