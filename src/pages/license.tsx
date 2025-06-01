bebccd5d73e894ac80cf7a25c1a2695b5f613060
98yejin <o0o2211915@gmail.com>
2025-06-01 10:49:53 -0400

    Add LicensePage component to display favicon attribution and licensing information, enhancing transparency and compliance with copyright requirements.

---
+++ src/pages/license.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import PageLayout from "@/components/layout/PageLayout";
+import { useTimeThemeStore } from "@/store/useTimeThemeStore";
+import clsx from "clsx";
+export default function LicensePage() {
+  const { timeOfDay } = useTimeThemeStore();
+  const isNight = timeOfDay == "night";
+  const timeOfDayStyle = isNight
+    ? "bg-gray-900 text-white border-white"
+    : "bg-white/80 text-black border-black";
+
+  return (
+    <PageLayout>
+      <div className={clsx("space-y-4 border rounded-lg p-5", timeOfDayStyle)}>
+        <ul className="text-sm leading-relaxed list-disc pl-5">
+          <li>
+            Favicon graphic © 2020 Twitter, Inc and other contributors.
+            <ul className="list-disc pl-5 mt-1">
+              <li>
+                Source:{" "}
+                <a
+                  className="text-blue-600 underline"
+                  href="https://github.com/twitter/twemoji/blob/master/assets/svg/1f33f.svg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  https://github.com/twitter/twemoji/blob/master/assets/svg/1f33f.svg
+                </a>{" "}
+                (licensed under{" "}
+                <a
+                  className="text-blue-600 underline"
+                  href="https://creativecommons.org/licenses/by/4.0/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  CC-BY 4.0
+                </a>
+                )
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </PageLayout>
+  );
+}
